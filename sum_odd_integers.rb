c2e7996aebe49c87c5131a404156fa8d52ed993d
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-14 20:38:41 +0000

    sum_odd_integers.rb

--- sum_odd_integers.rb
+++ sum_odd_integers.rb
@@ -11,3 +11,15 @@
 # 16
 
 p "Enter at least 2 numbers, separated by spaces:"
+
+user_numbers = gets.chomp.split(" ")
+
+odd_numbers = Array.new
+
+user_numbers.each do |oddcheck|
+  if oddcheck.to_i.odd? == true
+    odd_numbers.push(oddcheck.to_i)
+  end
+end
+
+p odd_numbers.sum
