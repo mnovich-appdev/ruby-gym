950805cf7102b249145a983ab7cda6ae899ddade
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-14 21:28:47 +0000

    count_the.rb

--- count_the.rb
+++ count_the.rb
@@ -4,3 +4,15 @@
 #   and finally prints, "'the' appeared x times", where x is an Integer
 
 p "Enter a sentence:"
+
+sentence = gets.gsub(/[[:punct:]]/," ").chomp.split
+
+the_counter = 0
+
+sentence.each do |the_tester|
+  if the_tester == "the"
+    the_counter = the_counter + 1
+  end
+end
+
+p "'the' appeared #{the_counter} times"
