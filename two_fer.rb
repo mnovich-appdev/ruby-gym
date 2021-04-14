d7d14a58c9cad03608654e1d34a5dacaf5a0a99e
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-14 20:47:40 +0000

    two_fer.rb

--- two_fer.rb
+++ two_fer.rb
@@ -9,3 +9,11 @@
 # If no name is given, the result should be "One for you, one for me."
 
 p "Enter a name:"
+
+name = gets.chomp.capitalize
+
+if name != ""
+  p "One for #{name}, one for me."
+else
+  p "One for you, one for me."
+end
